3487fdcce487ae7d3066eaf2cf36da80e7769699
unknown <SenaManana@ARMDFPCCIFSD006.FormacionSena.RED>
2024-05-16 08:58:56 -0500

    First commit-creation of the stack of books

---
+++ Index.js
@@ -0,0 +1,19 @@
+class Libro {
+    constructor(titulo, autor, genero, idioma, precio, formato, isbn, descripcion, estado, ubicacion, fecha_publicacion, editorial, paginas, dimensiones, peso) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.idioma = idioma;
+        this.precio = precio;
+        this.formato = formato;
+        this.isbn = isbn;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.ubicacion = ubicacion;
+        this.fecha_publicacion = fecha_publicacion;
+        this.editorial = editorial;
+        this.paginas = paginas;
+        this.dimensiones = dimensiones;
+        this.peso = peso;
+    }
+}
